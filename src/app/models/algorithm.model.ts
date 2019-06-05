3266a336cc4623f6dec1871579171c9f44076900
SakshiShreya <shreyasakshi96@gmail.com>
2019-06-06 01:42:24 +0530

    Added interfaces for all query fields

---
+++ src/app/models/algorithm.model.ts
@@ -0,0 +1,14 @@
+import { Language } from './language.model';
+import { Category } from './category.model';
+import { Code } from './code.model';
+
+export class Algorithm {
+  id?: string;
+  name?: string;
+  description: string;
+  langs: Language[];
+  createdAt: Date;
+  updatedAt: Date;
+  category: Category;
+  codeSet: Code[];
+}
